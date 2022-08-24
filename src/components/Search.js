f7b372bdf94ac28e5fe1263738cca76d41416950
Matthew Henderson <info@matthewahenderson.com>
2022-08-24 19:04:32 -0400

    Added useEffect for making network request; added results to state

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,11 +1,27 @@
 import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 const Search = () => {
     const [term, setTerm] = useState('');
+    const [results, setResults] = useState([]);
     
     useEffect(() => {
+        const search = async () => {
+            const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
+                params: {
+                    action: 'query',
+                    list: 'search',
+                    origin: '*',
+                    format: 'json',
+                    srsearch: term
+                }
+            });
 
-    });
+            setResults(data);
+        };
+
+        search();
+    }, [term]);
 
     return (
         <div>
