52cb7fcea84200e33604b3c2ff373b20fdfec788
Matthew Henderson <info@matthewahenderson.com>
2022-08-24 22:44:48 -0400

    Finished useEffect; created array of results and rendered them on the page in the return

--- src/components/Search.js
+++ src/components/Search.js
@@ -1,10 +1,11 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { render } from '@testing-library/react';
 
 const Search = () => {
-    const [term, setTerm] = useState('');
+    const [term, setTerm] = useState('programming');
     const [results, setResults] = useState([]);
-    
+        
     useEffect(() => {
         const search = async () => {
             const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
@@ -17,11 +18,32 @@ const Search = () => {
                 }
             });
 
-            setResults(data);
+            setResults(data.query.search);
         };
 
-        search();
+        if (term) {
+            search();
+        }
     }, [term]);
+
+    const renderedResults = results.map((result) => {
+        return (
+            <div key={result.pageid} className="item">
+                <div className="right floated content">
+                    <a 
+                        className="ui button"
+                        href={`https://en.wikipedia.org?curid=${result.pageid}`}
+                    >
+                        Go
+                    </a>
+                </div>
+                <div className="content">
+                    <div className="header">{result.title}</div>
+                    <span dangerouslySetInnerHTML={{ __html: result.snippet }}></span>
+                </div>
+            </div>
+        );
+    });
 
     return (
         <div>
@@ -35,6 +57,7 @@ const Search = () => {
                     />
                 </div>
             </div>
+            <div className="ui celled list">{renderedResults}</div>
         </div>
     );
 }
