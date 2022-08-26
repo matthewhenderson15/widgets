2fb7d52fc62925336b2694e490ec36621f38a138
Matthew Henderson <info@matthewahenderson.com>
2022-08-26 00:44:06 -0400

    Added new Translate component

---
+++ src/components/Translate.js
@@ -0,0 +1,16 @@
+import React, { useState } from 'react';
+import Dropdown from './Dropdown';
+
+const options = [
+    
+]
+
+const Translate = () => {
+    return (
+        <div>
+            <Dropdown />
+        </div>
+    );
+};
+
+export default Translate;
