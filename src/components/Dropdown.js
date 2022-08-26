c3348375284a27fae5324c4f1fef2636358eff11
Matthew Henderson <info@matthewahenderson.com>
2022-08-26 00:00:27 -0400

    Added functionality to select an item and close the menu using useRef

--- src/components/Dropdown.js
+++ src/components/Dropdown.js
@@ -1,24 +1,47 @@
-import Reaact from 'react';
+import Reaact, { useState, useEffect, useRef } from 'react';
 
 const Dropdown = ({ options, selected, onSelectedChange }) => {
+    const [open, setOpen] = useState(false);
+    const ref = useRef();
+    
+    useEffect(() => {
+        document.body.addEventListener('click', (event) => {
+            if (ref.current.contains(event.target)) {
+                return;
+            }
+
+            setOpen(false);
+        },
+        { capture: true });
+    }, []);
+      
     const renderedOptions = options.map((option) => {
+        if (option.value === selected.value) {
+            return null;
+        }
+        
         return (
-            <div key={option.value} className="item">
+            <div 
+                key={option.value} 
+                className="item"
+                onClick={() => onSelectedChange(option)}
+            >
                 {option.label}
             </div>
         );
     });
-    
+
     return (
-        <div className="ui form">
+        <div ref={ref} className="ui form">
             <div className="field">
                 <label className="label">Select a Color</label>
-                <div className="ui selection dropdown visible active">
+                <div 
+                    onClick={() => setOpen(!open)} 
+                    className={`ui selection dropdown ${open ? 'visible active' : ''}`}
+                >
                     <i className="dropdown icon"></i>
-                    <div className="text">Select Color</div>
-                    <div className="menu visible transition">
-                        {renderedOptions}
-                    </div>
+                    <div className="text">{selected.label}</div>
+                    <div className={`menu ${open ? 'visible transition' : ''}`}>{renderedOptions}</div>
                 </div>
             </div>
         </div>
